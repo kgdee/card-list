75a1506b98e303d94168767d22e096509dd9f1e3
kgdee <kgdee7603@gmail.com>
2025-05-25 03:44:23 -0400

    commit

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-const projectName = "card-list"
-const addItemEl = document.querySelector(".add-item");
+const projectName = "card-list";
 const cardsEl = document.querySelector(".cards");
+const cardModal = document.querySelector(".card-modal");
 
 let cards = JSON.parse(localStorage.getItem(`${projectName}_cards`)) || [];
 
-document.addEventListener("DOMContentLoaded", function() {
-  displayCards()
-})
+document.addEventListener("DOMContentLoaded", function () {
+  displayCards();
+});
+
+function stopPropagation(event) {
+  event.stopPropagation();
+}
 
 function addNewCard() {
-  const nameInput = addItemEl.querySelector(".name-input");
-  const iconInput = addItemEl.querySelector(`.icon-input input[type="file"]`);
+  const nameInput = cardModal.querySelector(".name-input");
+  const iconInput = cardModal.querySelector(`.icon-input input[type="file"]`);
+  const descriptionInput = cardModal.querySelector(".description-input")
 
-  const itemName = nameInput.value.trim();
+  const itemName = nameInput.value
   const icon = iconInput.files[0] ? URL.createObjectURL(iconInput.files[0]) : "images/icon-default-64x64.png";
+  const description = descriptionInput.value
 
   if (!itemName) return;
 
   const newCard = {
     name: itemName,
     icon: icon,
+    description: description
   };
 
   cards.push(newCard);
-  localStorage.setItem(`${projectName}_cards`, JSON.stringify(cards))
+  localStorage.setItem(`${projectName}_cards`, JSON.stringify(cards));
 
-  displayCards()
+  displayCards();
 
   nameInput.value = "";
   iconInput.value = "";
@@ -35,10 +42,41 @@ function displayCards() {
   cardsEl.innerHTML = cards.map(
     (card) =>
       `
-      <div class="item">
+      <div class="item" onclick="toggleCardModal(${cards.indexOf(card)})">
         <img src="${card.icon}">
         <p class="item-text truncated">${card.name}</p>
       </div>
     `
-  )
+  );
+}
+
+function handleIconInput(file, element) {
+  const preview = element.querySelector("img")
+
+  if (file instanceof Blob) {
+    preview.src = URL.createObjectURL(file)
+  } else if (file) {
+    preview.src = file
+  } else {
+    preview.src = "images/icon-default-64x64.png"
+  }
+}
+
+function toggleCardModal(cardIndex) {
+  const nameInput = cardModal.querySelector(".name-input")
+  const descriptionInput = cardModal.querySelector(".description-input")
+  const iconInput = cardModal.querySelector(".icon-input")
+
+  nameInput.value = ""
+  descriptionInput.value = ""
+  handleIconInput(null, iconInput)
+
+  cardModal.classList.toggle("hidden")
+
+  if (isNaN(cardIndex)) return
+
+  const card = cards[cardIndex]
+  handleIconInput(card.icon, iconInput)
+  nameInput.value = card.name
+  descriptionInput.value = card.description
 }
