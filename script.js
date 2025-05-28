1e202df4bf01bb344b26fa3a3492a2d6e20fb280
kgdee <kgdee7603@gmail.com>
2025-05-28 19:12:12 -0400

    commit

--- script.js
+++ script.js
@@ -15,47 +15,49 @@ function stopPropagation(event) {
 }
 
 function addNewCard() {
-  const itemName = nameInput.value;
-  const icon = iconInput.files[0] ? URL.createObjectURL(iconInput.files[0]) : "images/icon-default-64x64.png";
-  const description = descriptionInput.value;
+  const card = CardModal.getCard();
+  if (!card.name) return;
 
-  if (!itemName) return;
-
-  const newCard = {
-    name: itemName,
-    icon: icon,
-    description: description,
-  };
-
-  cards.push(newCard);
+  cards.push(card);
   localStorage.setItem(`${projectName}_cards`, JSON.stringify(cards));
 
   displayCards();
-
-  nameInput.value = "";
-  iconInput.value = "";
 }
 
 function displayCards() {
-  cardsEl.innerHTML = cards.map(
-    (card) =>
-      `
+  cardsEl.innerHTML = cards
+    .map(
+      (card) =>
+        `
       <div class="item" onclick="CardModal.toggle(${cards.indexOf(card)})">
         <img src="${card.icon}">
         <p class="item-text truncated">${card.name}</p>
       </div>
     `
-  );
+    )
+    .join("");
 }
 
 const CardModal = (() => {
-  element = document.querySelector(".card-modal")
-  title = document.querySelector(".card-modal .title")
-  submitBtn = document.querySelector(".card-modal .submit")
-  nameInput = document.querySelector(".card-modal .name-input")
-  descriptionInput = document.querySelector(".card-modal .description-input")
-  iconOptions = document.querySelector(".card-modal .icon-options")
-  iconInput = document.querySelector(".card-modal .icon-input")
+  const element = document.querySelector(".card-modal");
+  const title = document.querySelector(".card-modal .title");
+  const submitBtn = document.querySelector(".card-modal .submit");
+  const nameInput = document.querySelector(".card-modal .name-input");
+  const descriptionInput = document.querySelector(".card-modal .description-input");
+  const iconOptions = document.querySelector(".card-modal .icon-options");
+  const iconInput = document.querySelector(".card-modal .icon-input");
+  const iconFileInput = document.querySelector("input[type=file]")
+
+  iconInput.oninput = () => displayIconInput();
+
+  function getCard() {
+    const card = {
+      name: nameInput.value,
+      description: descriptionInput.value,
+      icon: iconFileInput.files.length > 0 ? URL.createObjectURL(iconFileInput.files[0]) : "images/icon-default-64x64.png",
+    };
+    return card;
+  }
 
   function toggle(cardIndex) {
     iconInput.querySelector("input[type=file]").value = null;
@@ -65,36 +67,27 @@ const CardModal = (() => {
     descriptionInput.value = "";
 
     displayIconOptions();
-    handleIconInput(null, iconInput);
 
-    element.classList.toggle("hidden");
+    if (isFinite(cardIndex)) {
+      const selectedCard = cards[cardIndex];
 
-    if (isNaN(cardIndex)) return;
+      title.textContent = `Update ${selectedCard.name}`;
+      submitBtn.textContent = "Update";
 
-    const card = cards[cardIndex];
-
-    title.textContent = `Update ${card.name}`;
-    submitBtn.textContent = "Update";
-
-    handleIconInput(card.icon, iconInput);
-    nameInput.value = card.name;
-    descriptionInput.value = card.description;
-  }
-
-  function handleIconInput(file, element) {
-    const preview = element.querySelector("img");
-
-    if (file instanceof Blob) {
-      preview.src = URL.createObjectURL(file);
-    } else if (file) {
-      preview.src = file;
-    } else {
-      preview.src = "images/icon-default-64x64.png";
+      nameInput.value = selectedCard.name;
+      descriptionInput.value = selectedCard.description;
     }
 
-    preview.classList.toggle("hidden", !file);
-    element.querySelector("i").classList.toggle("hidden", file);
-    element.style.border = file ? "2px solid" : "2px dashed";
+    displayIconInput();
+    element.classList.toggle("hidden");
+  }
+
+  function displayIconInput() {
+    const card = getCard();
+    iconInput.querySelector("img").src = card?.icon ? card.icon : "";
+    iconInput.querySelector("img").classList.toggle("hidden", !card?.icon);
+    iconInput.querySelector("i").classList.toggle("hidden", !!card?.icon);
+    iconInput.style.border = card?.icon ? "2px solid" : "2px dashed";
   }
 
   function displayIconOptions() {
@@ -122,7 +115,7 @@ const CardModal = (() => {
       .join("");
   }
 
-  return { toggle };
+  return { toggle, getCard };
 })();
 
 function toggleTheme(force = undefined) {
